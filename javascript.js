e8bd9d092592edb859f1f7a87b7c58f6e09f71b4
JohnJaeger <jmjaeger10@gmail.com>
2024-10-25 16:42:57 -0400

    Add round win and game win game logic
    - Core game logic was added to allow players to win rounds
    and ultimately win games.
    - A method that checks for round wins was added as well as a
    method that checks for game wins.
    - A method that processes round wins was added as well as a method
    that processes game wins.

--- javascript.js
+++ javascript.js
@@ -23,8 +23,53 @@ const game = (function(){
         }
     }
 
+    const gameLogic = {
+        winningPatterns:  [["TL", "TM", "TR"],
+                           ["ML", "MM", "MR"],
+                           ["BL", "BM", "BR"],
+                           ["TL", "ML", "BL"],
+                           ["TM", "MM", "BM"],
+                           ["TR", "MR", "BR"],
+                           ["TL", "MM", "BR"],
+                           ["BL", "MM", "TR"]],
+        checkRoundWin: function(){
+            for (winningPattern of this.winningPatterns){
+                const [a, b, c] = winningPattern;
+                if (gameBoard.cells[a] === gameBoard.cells[b] &&
+                    gameBoard.cells[b] === gameBoard.cells[c] &&
+                    gameBoard.cells[a] !== ''){
+                    return gameBoard.cells[a];
+                } else {
+                    return null;
+                }
+            }
+        },
+        roundWin: function(){
+            for (player of players){
+                if (player.symbol === this.checkRoundWin()){
+                    player.points += 1;
+                }
+            }
+        },
+        checkGameWin: function(){
+            for (player of players){
+                if (player.points === 3){
+                    return player;
+                }
+            }
+        },
+        gameWin: function(){
+            for (player of players){
+                if (player === this.checkGameWin()){
+                    console.log(player + " Wins!");
+                }
+            }
+        }
+    }
+
     return {
         gameBoard,
-        players
+        players,
+        gameLogic
     };
 })();
