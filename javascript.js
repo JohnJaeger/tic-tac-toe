0b82be844644a7d4e2bf62256c4524b3a681e71d
JohnJaeger <jmjaeger10@gmail.com>
2024-10-25 14:40:18 -0400

    Create gameBoard object with gameBoard data and gameBoard reset
    - This was done to store gameBoard data and allow users the ability
    to reset the gameBoard.

---
+++ javascript.js
@@ -0,0 +1,15 @@
+const game = (function(){
+    const gameBoard = {
+        cells: { TL: '', TM: '', TR: '',
+                 ML: '', MM: '', MR: '',
+                 BL: '', BM: '', BR: '' },
+        reset: function(){
+            for (let prop in this.cells){
+                if (this.cells.hasOwnProperty(prop)){
+                    this.cells[prop] = '';
+                }
+            }
+        },
+    };
+    return {gameBoard};
+})();
