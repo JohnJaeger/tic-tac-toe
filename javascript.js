4e5f259ab2f7e35a19d8f299a66b938331d332ca
JohnJaeger <jmjaeger10@gmail.com>
2024-10-27 18:04:52 -0400

    Update user interface to one that is more interactive
    - User interface was updated to a new interactive user interface.
    - User interface was minimally styled in order to show positioning
    and layout.
    - Edits were made to gameController in order to include new
    interface.

--- javascript.js
+++ javascript.js
@@ -6,12 +6,9 @@ const game = (function(){
         update: function(symbol, choice){
             this.cells[choice] = symbol;
         },
-        display: function(){
-            console.log(this.cells.TL + " | " + this.cells.TM + " | " + this.cells.TR);
-            console.log("----------");
-            console.log(this.cells.ML + " | " + this.cells.MM + " | " + this.cells.MR);
-            console.log("----------");
-            console.log(this.cells.BL + " | " + this.cells.BM + " | " + this.cells.BR);
+        isFull: function(){
+            const allFull = Object.values(this.cells).every(value => value !== '');
+            return allFull;
         },
         reset: function(){
             for (let prop in this.cells){
@@ -76,53 +73,129 @@ const game = (function(){
     }
 
     const gameController = {
-        getPlayerInput: function(player){
-            const playerChoice = prompt(player.name + " what is your choice?", "").toUpperCase();
-            return {player, playerChoice};
+        currentPlayer: null,
+        getPlayerInput: function() {
+            return new Promise((resolve) => {
+                this.resolveInput = resolve;
+            });
         },
-        roundWin: function(player){
-            player.points += 1;
-        },
-        gameWin: function(player){
-            console.log(player.name + " Wins!");
-        },
-        playRound: function(board, players, gameRules){
-            for (const player of players){
-                console.log(player.name + ": " + player.points);
-            }
-            outerLoop: while (true){
-                innerLoop: for (const player of players){
-                    board.display();
-                    let playerInput = this.getPlayerInput(player);
-                    board.update(playerInput.player.symbol, playerInput.playerChoice);
-                    let roundWinStatus = gameRules.checkRoundWin(board, players);
-                    if (roundWinStatus !== null){
-                        this.roundWin(roundWinStatus);
-                        board.display();
-                        board.reset();
-                        break outerLoop;
-                    } else {
-                        continue;
+        makeMove: function(board, event) {
+            if (event.target.matches('.game-board-cell')) {
+                if (this.currentPlayer) {
+                    const cellId = event.target.id;
+                    if (board.cells[cellId] === '') {
+                        if (this.resolveInput) {
+                            this.resolveInput(cellId);
+                            this.resolveInput = null;
+                        }
                     }
                 }
             }
         },
-        playGame: function(board, players, gameRules){
-            while (true){
-                this.playRound(board, players, gameRules);
+        roundWin: function(player){
+            player.points += 1;
+        },
+        gameWin: function(player, UIController){
+            UIController.displayGameWin(player);
+        },
+        resetGame: function(board, players, UIController){
+            board.reset();
+            for (const player of players){
+                player.reset();
+            }
+            UIController.update(board, players);
+        },
+        playRound: async function(board, players, gameRules, UIController){
+            outerLoop: while (true){
+                innerLoop: for (const player of players){
+                    this.currentPlayer = player;
+                    let playerChoice = await this.getPlayerInput();
+                    board.update(player.symbol, playerChoice);
+                    UIController.update(board, players);
+                    let roundWinStatus = gameRules.checkRoundWin(board, players);
+                    if (roundWinStatus !== null) {
+                        this.roundWin(roundWinStatus);
+                        board.reset();
+                        UIController.update(board, players);
+                        return;
+                    }
+                    if (board.isFull() === true){
+                        board.reset();
+                        UIController.update(board, players);
+                    }
+                }
+            }
+        },
+        playGame: async function(board, players, gameRules, UIController) {
+            this.resetGame(board, players, UIController);
+            while (true) {
+                await this.playRound(board, players, gameRules, UIController);
                 let gameWinStatus = gameRules.checkGameWin(players);
-                if (gameWinStatus !== null){
-                    this.gameWin(gameWinStatus);
+                if (gameWinStatus !== null) {
+                    this.gameWin(gameWinStatus, UIController);
                     break;
                 }
             }
         }
     };
 
+    const UIController = {
+        elements: {
+            mainTitle: document.querySelector(".main-title"),
+            playerOneScore: document.querySelector("#player-one-score"),
+            playerTwoScore: document.querySelector("#player-two-score"),
+            gameBoard: document.querySelector(".game-board"),
+            TL: document.querySelector("#TL"),
+            TM: document.querySelector("#TM"),
+            TR: document.querySelector("#TR"),
+            ML: document.querySelector("#ML"),
+            MM: document.querySelector("#MM"),
+            MR: document.querySelector("#MR"),
+            BL: document.querySelector("#BL"),
+            BM: document.querySelector("#BM"),
+            BR: document.querySelector("#BR")
+        },
+        update: function(board, players){
+            for (const player of players){
+                if (player.name === "Player 1"){
+                    this.elements.playerOneScore.textContent = "Player 1: " + player.points;
+                }
+                if (player.name === "Player 2"){
+                    this.elements.playerTwoScore.textContent = "Player 2: " + player.points;
+                }
+            }
+            for (const cellkey in board.cells){
+                if (board.cells.hasOwnProperty(cellkey) &&
+                    this.elements.hasOwnProperty(cellkey)){
+                    this.elements[cellkey].textContent = board.cells[cellkey];
+                }
+            }
+        },
+        displayGameWin: function(player){
+            this.elements.mainTitle.textContent = player.name + " Wins!"
+        },
+        initializeEventListeners: function(board, gameController) {
+            this.elements.gameBoard.addEventListener('click', (event) => {
+                gameController.makeMove(board, event);
+            });
+        }
+    }
+
+    UIController.initializeEventListeners(gameBoard, gameController);
+
+    const returnObject = {
+        start: function(){
+            gameController.playGame(gameBoard, players, gameRules, UIController);
+        },
+        reset: function(){
+            gameController.resetGame(gameBoard, players, UIController);
+        }
+    };
+
+    const {start, reset} = returnObject;
+
     return {
-        gameBoard,
-        players,
-        gameRules,
-        gameController
+        start,
+        reset
     };
 })();
